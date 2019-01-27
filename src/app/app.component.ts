9e9392abf67689a10f88ffc5160e063e5b4aa308
daaren-urthling <daaren.urthling@hotmail.com>
2019-01-27 17:06:37 +0100

    managed alert for errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,15 @@ enum ConnectionStatus { connecting, connected, noConnection }
 export class AppComponent implements OnInit {
   constructor(private _httpService: Http) { }
   connStatus: ConnectionStatus = ConnectionStatus.connecting;
+  connMessages: string = "";
   connectionStatus = ConnectionStatus;
    ngOnInit() {
       this._httpService.get('/api/loginManager/isAlive').subscribe(values => {
         var isAlive: boolean = values.json() as boolean;
         this.connStatus = isAlive ? ConnectionStatus.connected : ConnectionStatus.noConnection;
+      }, (error) => {
+        this.connStatus = ConnectionStatus.noConnection;
+        this.connMessages = error.statusText + ' - ' + error._body;
       });
    }
 }
